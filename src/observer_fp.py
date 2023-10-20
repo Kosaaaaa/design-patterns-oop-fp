9def7bad3efabf0b1ceb92f35ad4a27e928baca1
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2023-10-20 22:34:35 +0200

    add observer_fp.py

---
+++ src/observer_fp.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from collections.abc import Callable, Sequence
+
+
+def update_observer1(value: str) -> None:
+    print(f"Observer 1 received {value=}")
+
+
+def update_observer2(value: str) -> None:
+    print(f"Observer 2 received {value=}")
+
+
+UpdateFn = Callable[[str], None]
+
+
+def notify(update_fns: Sequence[UpdateFn], value: str) -> None:
+    for update_fn in update_fns:
+        update_fn(value)
+
+
+def main() -> None:
+    update_fns = [update_observer1, update_observer2]
+    notify(update_fns, "Some data")
+
+
+if __name__ == "__main__":
+    main()
